52f516b77b9e95363e20b512e7975fdaa930e73d
Mark Lubkowitz <mail@mark-lubkowitz.de>
2020-04-26 22:26:06 +0200

    Mixin variant with Object.assign added

---
+++ examples/longshort.assign.js
@@ -0,0 +1,33 @@
+const hooksMixin = require('../index.object.js')
+class LongShort {}
+Object.assign(LongShort.prototype, hooksMixin)
+let ls = new LongShort()
+
+ls.latch(
+    'shorten',
+    result => { return result === 'Read-eval-print loop' ? 'REPL' : result }
+)
+ls.at(
+    'shorten',
+    result => { return result === 'Javascript' ? 'JavaS.' : result }
+)
+ls.at(
+    'shorten',
+    result => { return result === 'JavaS.' ? 'JS' : result }
+)
+ls.latch(
+    'lengthen',
+    result => { return result === 'REPL' ? 'Read-eval-print loop' : result }
+)
+ls.at(
+    'lengthen',
+    result => { return result === 'JS' ? 'Javascript' : result }
+)
+
+console.log('Test =>', ls.hook('shorten', 'Test'))
+console.log('Javascript =>', ls.hook('shorten', 'Javascript'))
+console.log('Read-eval-print loop =>', ls.hook('shorten', 'Read-eval-print loop'))
+
+console.log('Test =>', ls.hook('lengthen', 'Test'))
+console.log('JS =>', ls.hook('lengthen', 'JS'))
+console.log('REPL =>', ls.hook('lengthen', 'REPL'))
---
+++ index.object.js
@@ -0,0 +1,79 @@
+/*
+Hooks are a concept of modifying the behaviour of a program by adding code without changing the original code. This externally code is provided via latches, which are actively collected by the program. Latches can provide and run their own code, or change what the program is doing or outputting by default.
+*/
+
+let errorMessages = {
+    1:  `.latch() expects at least two arguments, a hook name and a callback.`,
+    2:  '.latch() expects a string for the hook name.',
+    3:  '.latch() expects a function for the latch callback.',
+    10: '.unlatch() expects exactly two arguments, the latch id and the hook name.',
+    11: '.unlatch() expects a Symbol for the latch id.',
+    12: '.unlatch() expects a string for the hook name.',
+    20: '.hook() expects at least two arguments, the hook name and the initial result.',
+    21: '.hook() expects a string for the hook name.'
+}
+
+let hooksMixin = {
+    _hooks: {},
+
+    at: function () {
+        this.latch.apply(this, arguments)
+    },
+
+    latch: function (hookName, latchCallback, ...latchParams) {
+        //  check
+        if (arguments.length < 2)
+            throw new Error(errorMessages[1])
+        if (typeof arguments[0] !== 'string')
+            throw new TypeError(errorMessages[2])
+        if (typeof arguments[1] !== 'function')
+            throw new TypeError(errorMessages[3])
+
+        //  register hook
+        if (hookName in this._hooks === false)
+            this._hooks[hookName] = []
+
+        //  add latch
+        let latchId = Symbol(`Latch for the hook '${hookName}'`)
+            this._hooks[hookName].push({
+                id: latchId,
+                fn: latchCallback,
+                pr: latchParams
+            })
+
+        return latchId
+    },
+
+    unlatch: function (latchId, hookName) {
+        //  check
+        if (arguments.length !== 2)
+            throw new Error(errorMessages[10])
+        if (typeof arguments[0] !== 'symbol')
+            throw new TypeError(errorMessages[11])
+        if (typeof arguments[1] !== 'string')
+            throw new TypeError(errorMessages[12])
+        
+        //  remove latch
+        if (hookName in this._hooks)
+            this._hooks[hookName] = this._hooks[hookName].filter(latch => latch.id !== latchId)
+    },
+
+    hook: function (hookName, result, ...hookParams) {
+        //  check
+        if (arguments.length < 2)
+            throw new Error(errorMessages[20])
+        if (typeof arguments[0] !== 'string')
+            throw new TypeError(errorMessages[21])
+
+        //  pick up latches
+        if (hookName in this._hooks) {
+            this._hooks[hookName].forEach(latch => {
+                result = latch.fn.apply(this, hookParams.concat(result, latch.pr))
+            })
+        }
+
+        return result
+    }
+}
+
+module.exports = hooksMixin
